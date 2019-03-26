5cec8630ba4f954be2ee3df8f53d83bdae98d8be
heiuchi23 <lotteyoushowfranco4@gmail.com>
2019-03-27 00:33:58 +0900

    Upload result CSV to AmazonS3

--- auto-work.sh
+++ auto-work.sh
@@ -6,3 +6,7 @@ cp /home/ubuntu/nem/nis/data/nis5_mainnet.h2.db /home/ubuntu/nem/nis/data/nis5_m
 /usr/bin/java -cp .:h2-1.4.196.jar CsvCreator
 /usr/bin/Rscript summarise_blocks.R
 /usr/bin/python3 auto-tweet.py
+
+/usr/local/bin/aws s3 cp daily.csv s3://nemdashboard.com/ --acl public-read
+/usr/local/bin/aws s3 cp transfers.csv s3://nemdashboard.com/ --acl public-read
+
