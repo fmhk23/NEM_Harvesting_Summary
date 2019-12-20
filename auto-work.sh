557cc894005ce6ca9358b10c813dbd1645aac01f
heiuchi23 <lotteyoushowfranco4@gmail.com>
2019-12-20 20:19:46 +0900

    Modify dashboard url

--- auto-work.sh
+++ auto-work.sh
@@ -3,7 +3,7 @@
 cd /home/ubuntu/nem_harvest
 cp /home/ubuntu/nem/nis/data/nis5_mainnet.h2.db /home/ubuntu/nem/nis/data/nis5_mainnet2.h2.db
 
-/usr/bin/java -cp .:h2-1.4.196.jar CsvCreator
+/usr/bin/java -Xms5G -cp .:h2-1.4.196.jar CsvCreator
 /usr/bin/Rscript summarise_blocks.R
 /usr/bin/python3 auto-tweet.py
 
